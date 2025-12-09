32b902bf26128977bd8586de2febb4a29d64ce4b
Noah Contardo <noahcgithub@gmail.com>
2025-12-09 14:20:21 -0500

    Enhance login.html with improved button styles and layout adjustments for better user experience. Updated admin and about links for consistency.

--- Back-end/venv/bin/activate.fish
+++ Back-end/venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/me/Desktop/Bytes-and-Blowdry/Back-end/venv
+set -gx VIRTUAL_ENV /home/me/Bytes-and-Blowdry/Back-end/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
